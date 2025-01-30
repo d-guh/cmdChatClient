1a85f93ed5c371128299abf4491a064e604727d7
Desktop D-Guy <ddcool535@gmail.com>
2025-01-29 20:07:31 -0500

    Implemented simple OOP single input/output chat.

--- client.cpp
+++ client.cpp
@@ -1,4 +1,51 @@
+// client.cpp
+// cmdChatClient to handle send and recv.
+
 #include "client.h"
+
+void Client::start() {
+    if (DEBUG) cout << "DEBUG: Starting client..." << endl;
+    connect_to_server();
+    recieve_thread = thread(&Client::recieve_messages, this);
+    send_messages();
+}
+
+void Client::connect_to_server() {
+    if (DEBUG) cout << "DEBUG: Connecting to server at " << server_address << ":" << server_port << endl;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in serv_addr{};
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(server_port);
+    inet_pton(AF_INET, server_address.c_str(), &serv_addr.sin_addr);
+
+    if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        cerr << "Connection failed!" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    if (DEBUG) cout << "DEBUG: Connected to server." << endl;
+}
+
+void Client::send_messages() {
+    string message;
+    while (true) {
+        getline(cin, message);
+        send(sockfd, message.c_str(), message.size(), 0);
+        if (DEBUG) cout << "DEBUG: Sent message: " << message << endl;
+    }
+}
+
+void Client::recieve_messages() {
+    char buffer[1024];
+    while (true) {
+        int bytes_received = recv(sockfd, buffer, sizeof(buffer) - 1, 0);
+        if (bytes_received > 0) {
+            buffer[bytes_received] = '\0';
+            lock_guard<mutex> lock(cout_mutex);
+            cout << buffer << endl;
+            if (DEBUG) cout << "DEBUG: Received message: " << buffer << endl;
+        }
+    }
+}
 
 int main(int argc, char* argv[]) {
     string input = "", username = "user", address = "127.0.0.1", port = "12345";
@@ -23,7 +70,9 @@ int main(int argc, char* argv[]) {
     }
     if (DEBUG) cout << "DEBUG: username: " << username << ", address: " << address << ", port: " << port << endl;
 
-    // TODO: Implement OOP for handling connections asyncronously
+    Client client(username, address, stoi(port));
 
-    // TODO: Implement ncurses
+    client.start();
+
+    return 0;
 }
--- client.h
+++ client.h
@@ -1,12 +1,38 @@
+// client.h
+// Headerfile for cmdChatClient.
+
+#ifndef CLIENT_H
+#define CLIENT_H
+
 #include <iostream>
 #include <string>
-#include <sys/socket.h>
+#include <thread>
+#include <mutex>
+#include <vector>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <cstring>
-
-using namespace std;
 
 #define DEBUG 1
 
-// TODO: Implement OOP headers for handling connections asyncronously
+using namespace std;
+
+class Client {
+public:
+    Client(const string& username, const string& address, int port) : client_username(username), server_address(address), server_port(port) {}
+    void start();
+
+private:
+    string client_username;
+    string server_address;
+    int server_port;
+    int sockfd;
+    thread recieve_thread;
+    mutex cout_mutex;
+
+    void connect_to_server();
+    void send_messages();
+    void recieve_messages();
+};
+
+#endif
